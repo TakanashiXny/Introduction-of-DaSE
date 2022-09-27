b67fe06cf76d8ad3469c0fcb62b229bd3d9a8d72
XiaoNiaoYou <111627036+TakanashiXny@users.noreply.github.com>
2022-09-27 20:22:07 +0800

    Update pi.py

--- 9.27实验任务/pi.py
+++ 9.27实验任务/pi.py
@@ -2,36 +2,47 @@ import random
 import math
 
 
-# method 1: probability
+# method 1: 无穷级数
 def pi1():
-    total = 100000
+    cnt = 0
     Sum = 0
-    for i in range(total):
+    while abs(Sum - math.pi ** 2 / 6) > 0.0000000001:
+        cnt += 1
+        Sum += 1 / (cnt ** 2)
+    result = math.sqrt(6 * Sum)
+    print("%.10f" % result)
+    print(f"进行了{cnt}次")
+
+
+# method 2: arc-tan1
+def pi2():
+    Sum = 0
+    cnt = 0
+    while abs(Sum - math.pi / 4) > 0.000000001:
+        Sum += (-1) ** cnt / (2 * cnt + 1)
+        cnt += 1
+    result = Sum * 4
+    print("%.10f" % result)
+    print(f"进行了{cnt}次")
+
+
+# method 3: 蒙特卡罗概率
+def pi3():
+    Sum = 0
+    total = 1
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    if x ** 2 + y ** 2 < 1:
+        Sum = 1
+    while abs(Sum / total * 4 - math.pi) > 0.000000001:
         x = random.uniform(-1, 1)
         y = random.uniform(-1, 1)
         if x ** 2 + y ** 2 < 1:
             Sum += 1
-    prob = Sum / total
-    result = prob * 4
+        total += 1
+    result = Sum / total * 4
     print("%.10f" % result)
-
-
-# method 2: Fourier
-def pi2():
-    Sum = 0
-    for i in range(1, 1000):
-        Sum += 1 / (i * i)
-    result = math.sqrt(6 * Sum)
-    print("%.10f" % result)
-
-
-# method 3: arc-tan1
-def pi3():
-    Sum = 0
-    for i in range(1000):
-        Sum += (-1) ** i / (2 * i + 1)
-    result = 4 * Sum
-    print("%.10f" % result)
+    print(f"进行了{total}次")
 
 
 if __name__ == '__main__':
